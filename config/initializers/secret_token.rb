d6dbc24348ebc826f6ed404128774da587d02ed8
chubbson <chubbson@gmail.com>
2011-03-08 22:23:40 +0100

    railorCoaster Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailorCoaster::Application.config.secret_token = '0c4a78b54ae5ce395c9f4af8eff5a7d04f6c7d6c92886c28392f65fca074f2c507e7791107315c2957dbd2586d80c9b086319c6df28ed26ae1592e8d5b92249e'
