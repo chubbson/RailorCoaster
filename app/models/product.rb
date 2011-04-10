070e4c5997c3aeda6988d49c8c12bf3415397979
chubbson <chubbson@gmail.com>
2011-04-10 18:58:12 +0200

    chapter 9

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,10 @@
 class Product < ActiveRecord::Base
         default_scope :order => 'title'
 
+	has_many :line_items
+
+	before_destroy :ensure_not_referenced_by_any_line_item
+
 	validates :title, :description, :image_url, :presence => true
 	validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
 	validates :title, :uniqueness => true
@@ -8,5 +12,16 @@ class Product < ActiveRecord::Base
 		:with 	=> %r{\.(gif|jpg|png)$}i, 
 		:message => 'must be a URL for GIF, JPG or PNG image.'
 	}
-end
 
+	private 
+
+		#ensure that there are no line referencing this product
+		def ensure_not_referenced_by_any_line_item
+			if line_items.count.zero?
+				return true
+			else 
+				errors.add(:base, 'Line Items present')
+				return false
+			end
+		end
+end
