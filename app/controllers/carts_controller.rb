939769ba53a2d0da95f0cebfa8650ba6049f0295
chubbson <chubbson@gmail.com>
2011-06-07 16:00:49 +0200

    Chapter 11

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -77,11 +77,11 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.xml
   def destroy
-    @cart = Cart.find(params[:id])
+    @cart = current_cart
     @cart.destroy
 
     respond_to do |format|
-      format.html { redirect_to(carts_url) }
+      format.html { redirect_to(store_url) }
       format.xml  { head :ok }
     end
   end
